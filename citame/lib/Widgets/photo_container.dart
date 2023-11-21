9b19ef2ac71088e15e0816d0775efaa24a7b6f71
Dacfnetic <dacf9x@gmail.com>
2023-11-21 12:33:07 -0600

    Agregué el selector de imagenes en la pagina para crear negocios

--- citame/lib/Widgets/photo_container.dart
+++ citame/lib/Widgets/photo_container.dart
@@ -1,12 +1,45 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class EspacioParaSubirFotoDeNegocio extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class EspacioParaSubirFotoDeNegocio extends StatefulWidget {
   const EspacioParaSubirFotoDeNegocio({
     super.key,
   });
 
   @override
+  State<EspacioParaSubirFotoDeNegocio> createState() =>
+      _EspacioParaSubirFotoDeNegocioState();
+}
+
+class _EspacioParaSubirFotoDeNegocioState
+    extends State<EspacioParaSubirFotoDeNegocio> {
+  File? selectedImage;
+  @override
   Widget build(BuildContext context) {
+    Future pickImageFromGallery() async {
+      final returnedImage =
+          await ImagePicker().pickImage(source: ImageSource.gallery);
+      if (returnedImage != null) {
+        final camino = returnedImage.path;
+        setState(() {
+          selectedImage = File(camino);
+        });
+      }
+    }
+
+    Future pickImageFromCamera() async {
+      final returnedImage =
+          await ImagePicker().pickImage(source: ImageSource.camera);
+      if (returnedImage != null) {
+        final camino = returnedImage.path;
+        setState(() {
+          selectedImage = File(camino);
+        });
+      }
+    }
+
     return Container(
       margin: EdgeInsets.all(5),
       decoration: BoxDecoration(
@@ -16,24 +49,48 @@ class EspacioParaSubirFotoDeNegocio extends StatelessWidget {
       height: 200,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.end,
         children: [
-          Container(
-            height: 100,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
-              color: Colors.amberAccent,
-              /*image: DecorationImage(
-                  image: AssetImage('lib/assets/SplashScreen.jpg'),
-                  fit: BoxFit.fill,
-                ),*/
+          Expanded(
+            child: /*Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: Colors.red,
+                border: Border.all(width: 5),
+              ),*/
+                ClipRRect(
+              borderRadius: BorderRadius.circular(16),
+              child: selectedImage != null
+                  ? Image.file(
+                      selectedImage!,
+                      width: double.infinity,
+                      height: 230,
+                      fit: BoxFit.cover,
+                    )
+                  : Text('Sube una imagen prro'),
             ),
           ),
-          ElevatedButton(
-            onPressed: () {
-              //TODO: Anthony va a ver como incluir la foto del negocio
-            },
-            child: Text('Subir imagen'),
+          Row(
+            children: [
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: () {
+                    //TODO: Anthony va a ver como incluir la foto del negocio
+                    pickImageFromGallery();
+                  },
+                  child: Text('Subir imagen'),
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: () {
+                    //TODO: Anthony va a ver como incluir la foto del negocio
+                    pickImageFromCamera();
+                  },
+                  child: Text('Tomar foto'),
+                ),
+              ),
+            ],
           )
         ],
       ),
