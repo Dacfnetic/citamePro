057e46f16960dbd8be9dbdcd8ce5c5a1d0a8e745
Anthony <anthony_juarez_guerra@hotmail.com>
2023-12-07 00:18:37 -0600

    Workers y CRUD

--- citame_back/src/models/worker.model.js
+++ citame_back/src/models/worker.model.js
@@ -2,8 +2,12 @@ const {Schema, model} = require('mongoose')
 
 //Datos que se guardan en la BD
 const workerSchema = new Schema({
-
-    
+    name: {type: String, required: true},
+    email: {type:mongoose.Schema.Types.ObjectId, ref:'usuario',required:true},
+    imgPath:[Number],
+    salary :{type: Number},
+    horario: {type: String},
+    status: {type: Boolean}
 });
 
 module.exports = model('worker',workerSchema);
