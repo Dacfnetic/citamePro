2ae812f123279f74693c6789370041850d9c0566
Anthony <anthony_juarez_guerra@hotmail.com>
2023-12-10 23:03:48 -0600

    Workers work

--- citame_back/src/routes/services/services.controller.js
+++ citame_back/src/routes/services/services.controller.js
@@ -5,18 +5,43 @@ const services = require('../../models/services.model.js');
 
 async function getServices(req,res){
     
+    try{
+        business.findOne({email: req.get('email')})
+            .then(async(docs)=>{
+                if(docs.email == req.get('email')){
+                    const allServices = await services.find({nombreServicio: req.get('nombreServicio')});
+                    return res.status(200).json(allServices);
+                }
+            }).catch(e=>console.log(e));
+    }catch(e){
+        return res.status(404).json('Errosillo');
+    }  
+
+
 }
 
 async function postServices(req,res){
 
     try{
         
-
-
-
+        business.findOne({email: req.body.email})
+        .then(async (docs)=>{
+            if(docs != null){
+                console.log('Creando Servicio');
+                await services.create({
+                    nombreServicio: req.body.nombreServicio,
+                    businessCreatedBy : docs._id,
+                    precio: req.body.precio,
+                    imgPath: req.body.imgPath,
+                    descripcion: req.body.descripcion
+                });
+                return res.status(201).send({'sms':'Servicio creado'});
+            }
+            return res.status(202).send({'sms': 'El Servicio ya existe'});//Cambiar porque est[a raro]
+        });
     }catch(e){
         return res.status(404).json('Errosillo');
-    } 
+    }  
 
 };
 
--- citame_back/src/routes/workers/worker.controller.js
+++ citame_back/src/routes/workers/worker.controller.js
@@ -1,22 +1,39 @@
 //Importación de modelos de objetos
 const usuario = require('../../models/users.model.js');
 const business = require('../../models/business.model.js');
-const workers = require('../../models/worker.model.js');
+const workersModel = require('../../models/worker.model.js');
 
 async function postWorkers(req,res){
     try{
-        /* let existe = true;
-        workers.findOne({email: req.body.email})
-        .then(async (docs)=>{
-            if(docs == null){
-                existe = false;
-        }});*/
-        //if(existe) return res.status(201).send('El worker ya esta en el negocio');
-        usuario.findOne({emailUser: req.body.email})
-        .then(async (docs)=>{
+       
+        
+        let worker = [];  
+        await business.findOne({businessName: req.body.businessName, email: req.body.email})
+            .then((docs) => {
+                if(docs != null){
+                    worker = JSON.parse(JSON.stringify(docs.workersModel)); //id mongo 
+        }});
+        
+        const workersEmail = worker.map(async (e)=>{ 
+                
+            await workersModel.find({id : e}).then((docs) =>{
+                    return docs.email;
+                });
+              
+            
+            })
+        const workerExist = workersEmail.has(req.body.email);
+
+
+        if (workerExist == true){
+            return res.status(202).send('El trabajador ya esta en el negocio');
+        }else {
+
+            usuario.findOne({emailUser: req.body.email})
+            .then(async (docs)=>{
             if(docs != null){
                 console.log('Creando Trabajador');
-                await workers.create({
+                await workersModel.create({
                     id: docs._id,
                     name: req.body.name,
                     email: req.body.email,
@@ -28,19 +45,27 @@ async function postWorkers(req,res){
                 });
                 return res.status(201).send({'sms':'Trabajador creado'});
             }
-            return res.status(202).send({'sms': 'El Trabajador ya existe'});//Cambiar porque est[a raro]
-        });
+               
+            });
+
+        }
+        
     }catch(e){
         return res.status(404).json('Errosillo');
-    }  
+    }; 
+
 }
 
 async function deleteWorkers(req,res){
-    
+
+
+
+
 }
 
 module.exports  = {
 
-    postWorkers
+    postWorkers,
+    deleteWorkers
 
 }
