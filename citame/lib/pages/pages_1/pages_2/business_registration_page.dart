f3954cf2fb4f83fbbf2bb3f30a17fcff65985c17
DancingAlien96 <124532081+DancingAlien96@users.noreply.github.com>
2023-12-12 21:43:34 -0600

    icono de informacion, foto por defecto y borde en pagina de creacion de negocio

--- citame/lib/pages/pages_1/pages_2/business_registration_page.dart
+++ citame/lib/pages/pages_1/pages_2/business_registration_page.dart
@@ -64,7 +64,7 @@ class BusinessRegisterPage extends ConsumerWidget {
              
              
                  */
-
+              //media query
               Row(
                 children: [
                   Text(
